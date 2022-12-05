3a612779d02bdd2997a1c78e338ba62d8437dbe8
Carlos Daza <cdaza@ontop.ai>
2022-12-04 20:24:43 -0500

    Eureka registration

--- src/main/java/com/parking/user/UserApplication.java
+++ src/main/java/com/parking/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.parking.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 @EntityScan({ "com.parking.user.model.entity" })
 public class UserApplication {
